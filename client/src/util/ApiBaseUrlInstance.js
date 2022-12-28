6de089a822ffd2870bd91aa66cc682c201ed2dc2
Amrit <AmritMaan2001@gmail.com>
2022-12-27 23:51:21 -0500

    update axois request

--- client/src/util/ApiBaseUrlInstance.js
+++ client/src/util/ApiBaseUrlInstance.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 
 export const axoisInstance = axios.create({
-    baseURL: "http://localhost:5001/api/"
+    baseURL: "https://apus-ecommerce-api.onrender.com/api/"
 })
