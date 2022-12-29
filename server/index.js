281dffb44e330f8af9d6902f1f8408207a698c07
Amrit <AmritMaan2001@gmail.com>
2022-12-28 20:42:09 -0500

    cors issue fix

--- server/index.js
+++ server/index.js
@@ -6,9 +6,8 @@ const express = require("express");
 const app = express();
 const connectDatabase = require("./src/dbConnection");
 const cors = require("cors");
-const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
